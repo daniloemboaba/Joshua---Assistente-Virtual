cfb6e9dca7bdaab2b57b8bf457e015a2548c9868
daniloemboaba <danilo.emboaba@hotmail.com>
2022-10-12 18:01:22 -0300

    Correção do codigo

---
+++ nlu/model.py
@@ -0,0 +1,74 @@
+import yaml
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.utils import to_categorical
+
+
+data = yaml.safe_load(open('/home/danilo/Documentos/Joshua---Assistente-Virtual/nlu/train.yml', 'r', encoding='utf-8').read())
+
+inputs, outputs = [], []
+
+for command in data['commands']:
+    inputs.append(command['input'].lower())
+    outputs.append('{}\{}'.format(command['entity'], command['action']))
+
+# Processar texto: palavras, caracteres, bytes, sub-palavras
+
+
+# Mapear char-idx
+
+max_seq = max([len(bytes(x.encode('utf-8'))) for x in inputs])
+
+print('Maior seq:', max_seq)
+
+# Criar o dataset (numero de exemplos, tamanho da seq, num caracteres) one-hot
+
+# Input Data one hot encoding
+
+input_data = np.zeros((len(inputs), max_seq, 256),dtype:='float32')
+for i, inp in enumerate(inputs):
+    for k, ch in enumerate(bytes(inp.encode('utf-8'))):
+        input_data[i, k, int(ch)] = 1.0
+
+# Input data sparse
+
+#input_data = np.zeros((len(inputs), max_seq, dtype='int32')
+
+#for i, input in enumerate(inputs):
+    #for k, ch in enumerate(input):
+        #input_data[i, k, = chr2idx[ch]] = 1.0
+
+# Output Data
+
+labels = set(outputs)
+
+label2idx = {}
+idx2label = {}
+
+for k, label in enumerate(labels):
+    label2idx[label] = k
+    idx2label[k] = label
+
+output_data = []
+
+for output in outputs:
+    output_data.append(label2idx[output])
+
+output_data = to_categorical(output_data, len(output_data))
+
+print(output_data[0])
+
+model = Sequential()
+model.add(LSTM(128))
+model.add(Dense(len(output_data), activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
+model.fit(input_data, output_data, epochs=16)
+
+
+
+#print(inputs)
+#print(outputs)
